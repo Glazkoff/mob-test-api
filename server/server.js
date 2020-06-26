44c89e34abb13c47d354a7aa07158d39776bcdf8
Nikita Glazkov <zitrnik@gmail.com>
2020-06-26 10:13:44 +0300

    DEV: add 404 server path

--- server/server.js
+++ server/server.js
@@ -5,10 +5,17 @@ const port = process.env.PORT || 3000;
 let obj = {
   message: "Привет от Никиты!",
 };
+let objError = {
+  status: 404,
+  message: "Такого пути не сущетсвует!",
+};
+
 app.get("/api/message", (req, res) => {
   res.send(obj);
 });
-
+app.get("/api/*", (req, res) => {
+  res.send(obj);
+});
 app.listen(port, () =>
   console.log(`Сервер запущен на http://localhost:${port}`)
 );
